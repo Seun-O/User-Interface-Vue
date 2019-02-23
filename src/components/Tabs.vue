6b24acbea67bb9e5d2078697796ecb4e67cf378b
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-02-23 09:37:18 -0500

    Services page layout complete

---
+++ src/components/Tabs.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+      <div class="tab-links">
+        <div class="btn link active-tab" data-tab="pre-construction">
+          Pre-Construction
+        </div>
+        <div class="btn link" data-tab="construction">Construction</div>
+        <div class="btn link" data-tab="design-build">Design Build</div>
+        <div class="btn link" data-tab="sustainability">Sustainability</div>
+      </div>
+      <div class="services-content">
+        <div class="tab-content">
+          <div class="tab-text">
+            <h2 class="sub">Pre-Contruction</h2>
+            <p>
+              Completely synergize resource taxing relationships via premier
+              niche markets. Professionally cultivate one-to-one customer
+              service with robust ideas. Dynamically innovate resource-leveling
+              customer service for state of the art customer service.
+              <br />
+              <br />
+              Phosfluorescently engage worldwide methodologies with web-enabled
+              technology. Interactively coordinate proactive e-commerce via
+              process-centric "outside the box" thinking. Completely pursue
+              scalable customer service through sustainable potentialities.
+            </p>
+          </div>
+          <div class="tab-img">
+            <img
+              class="timg img-active"
+              data-tab="pre-construction"
+              src="../assets/services/services-tab-pre-construction-img.png"
+              alt=""
+            />
+            <img
+              class="timg"
+              data-tab="construction"
+              src="../assets/services/services-tab-construction-img.png"
+              alt=""
+            />
+            <img
+              class="timg"
+              data-tab="design-build"
+              src="../assets/services/services-tab-design-build-img.png"
+              alt=""
+            />
+            <img
+              class="timg"
+              data-tab="sustainability"
+              src="../assets/services/services-tab-sustainability-img.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Tabs"
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/Services.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <div class="service-head">
+      <img class="desk-img" src="../assets/services/services-jumbotron.png" alt="" />
+      <img
+        class="mobile-img"
+        src="../assets/services/services-mobile-jumbotron.png"
+        alt=""
+      />
+      <h2 class="jumbo">Services</h2>
+    </div>
+    <section id="services">
+      <div class="services-intro">
+        <p>
+          Services include: completely synergize resource taxing relationships
+          via premier niche markets. Professionally cultivate one-to-one
+          customer service with robust ideas. Dynamically innovate
+          resource-leveling customer service for state of the art customer
+          service.
+        </p>
+      </div>
+
+      <Tabs />
+    </section>
+  </div>
+</template>
+
+<script>
+import Tabs from "../components/Tabs"
+export default {
+  name: "Services",
+  components: {
+    Tabs,
+  }
+}
+</script>
